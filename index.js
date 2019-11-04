2281989210c646f59c1c5e89b69300defb298283
Steven Turner <steven.turner12x@gmail.com>
2019-11-03 21:27:28 -0600

    added js - not working for some reason

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict"
+
+const purple = document.getElementById('purple');
+// const body = document.getElementById('body');
+
+purple.addEventListener("click", function () {
+	document.body.style.backgroundColor = "firebrick";
+	}
+)
